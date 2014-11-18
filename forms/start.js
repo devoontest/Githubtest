b4578a1f0730bc936a9b105edcd6e80d91c5886b
dennis-devoon <dennis@devoon.nl>
2014-11-18 12:37:41 +0100

    new feature

--- forms/start.js
+++ forms/start.js
@@ -1,3 +1,43 @@
+/**
+ * @type {String}
+ *
+ * @properties={typeid:35,uuid:"0A1224AF-6C1E-4224-BB82-8D045EB360DA"}
+ */
+var html = "<!doctype html>                        \
+<html>                                             \
+<head>                                             \
+<meta charset='UTF-8'>                             \
+<title>Untitled Document</title>                   \
+</head>                                            \
+                                                   \
+<style>                                            \
+html {                                             \
+	width:100%;                                    \
+	height:100%;                                   \
+}                                                  \
+body {                                             \
+	width:100%;                                    \
+	height:100%;                                   \
+}                                                  \
+.centerblock {                                     \
+	width:128px;                                   \
+	height:auto;                                   \
+	text-align:center;                             \
+	margin:auto;                                   \
+	border:3px solid blue;                         \
+	color:blue;                                    \
+}                                                  \
+</style>                                           \
+<body>                                             \
+	<div class='centerblock'>                      \
+		<p>Hallo ik ben een tekt in een div.</p>   \
+		<p>Ik ben de tweede regel in die div.</p>  \
+		<br>                                       \
+		<p>Dit is het einde van de div</p>         \
+	</div>                                         \
+</body>                                            \
+</html>                                            \
+";
 
 /**
  * Perform the element default action.
